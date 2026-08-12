c8700170cb367f46eb9bd8fb795a30e03b3dca0e
Pranjal Chavda <pranjalchavda16@gmail.com>
2026-08-12 17:27:37 +0530

    Apply validateOrder middleware to order checkout route

--- server/src/routes/orderRoutes.js
+++ server/src/routes/orderRoutes.js
@@ -6,11 +6,12 @@ const {
   updateStatus
 } = require('../controllers/orderController');
 const { authenticateUser, authorizeSeller } = require('../middlewares/auth');
+const { validateOrder } = require('../middlewares/validate');
 
 const router = express.Router();
 
 // Learner order placement and history (Protected, any authenticated user)
-router.post('/', authenticateUser, checkout);
+router.post('/', authenticateUser, validateOrder, checkout);
 router.get('/', authenticateUser, listUserOrders);
 
 // Seller incoming orders and fulfillment (Protected, Seller only)
