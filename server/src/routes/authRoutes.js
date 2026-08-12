d26c3d6deef4cdd0cc3afbdd40ccf39a506f313c
Pranjal Chavda <pranjalchavda16@gmail.com>
2026-08-12 17:27:04 +0530

    Apply validateRegister and validateLogin middlewares to authRoutes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { register, login, getMe, upgradeToSeller, updateProfile } = require('../controllers/authController');
 const { authenticateUser } = require('../middlewares/auth');
+const { validateRegister, validateLogin } = require('../middlewares/validate');
 
 const router = express.Router();
 
 // Public auth routes
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateRegister, register);
+router.post('/login', validateLogin, login);
 
 // Protected profile routes
 router.get('/me', authenticateUser, getMe);
